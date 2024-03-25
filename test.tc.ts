8a151717a879f104209696909287af746316f811
Hauke Thorenz <hauke.thorenz@gti.de>
2024-03-25 10:55:27 +0100

    cleanup

--- test.tc.ts
+++ test.tc.ts
@@ -1,17 +1,22 @@
-import { ClientFunction, Selector, fixture, test } from "testcafe";
-import { 
-	getRandomIntInRange,
+import { readFileSync } from "fs";
+import { join as joinPath } from "path";
+import { PNG } from "pngjs";
+import { ClientFunction, fixture, Selector, test } from "testcafe";
+import {
 	getWindowDevicePixelRatio,
 	getWindowInnerHeight,
 	getWindowInnerWidth,
-	mkValidPath
+	mkValidPath,
 } from "./tools";
-import { PNG } from "pngjs";
-import {readFileSync} from "fs";
-import {join as joinPath} from "path";
 
-const _targetWidth = 800;
-const _targetHeight = 600;
+const targetWidth = 640;
+const targetHeight = 480;
+const targetDimensions = {width: targetWidth, height: targetHeight};
+
+const getWindowInnerDimensions = ClientFunction(() => ({width: window.innerWidth, height: window.innerHeight}));
+
+// set size to something else so resize is necessary
+const setSizeToSomethingElse = async (t: TestController) => t.resizeWindow(targetWidth + 13, targetHeight + 13);
 
 fixture("screenshot")
 	.page("about:blank")
@@ -27,91 +32,71 @@ test("openBrowser()", async t => {
 });
 
 test("resizeWindow()", async t => {
-	const targetWidth = _targetWidth + getRandomIntInRange(1, 100);
-	const targetHeight = _targetHeight + getRandomIntInRange(1, 100);
-	
-	await t.expect(getWindowInnerWidth()).notEql(targetWidth, "Width not expected to be targetWidth yet!");
-	await t.expect(getWindowInnerHeight()).notEql(targetHeight, "Height not expected to be targetHeight yet!");
-    
+	await t.expect(getWindowInnerDimensions()).notEql(
+		targetDimensions,
+		"Window not expected to be have targetDimensions yet!",
+	);
+
 	await t.resizeWindow(targetWidth, targetHeight);
-    
-	await t.expect(getWindowInnerWidth()).eql(targetWidth, "Width not properly set to targetWidth!");
-	await t.expect(getWindowInnerHeight()).eql(targetHeight, "Height not properly set to targetHeight!");
-});
+
+	await t.expect(getWindowInnerDimensions()).eql(targetDimensions, "Window not resized properly!");
+}).after(setSizeToSomethingElse);
 
 test("maximizeWindow()", async t => {
-	const targetWidth = _targetWidth + getRandomIntInRange(1, 100);
-	const targetHeight = _targetHeight + getRandomIntInRange(1, 100);
-
 	await t.resizeWindow(targetWidth, targetHeight);
-    
-	await t.expect(getWindowInnerWidth()).eql(targetWidth, "Width not properly set to targetWidth!");
-	await t.expect(getWindowInnerHeight()).eql(targetHeight, "Height not properly set to targetHeight!");
+	await t.expect(getWindowInnerDimensions()).notEql(targetDimensions, "Window not resized properly!");
 
 	await t.maximizeWindow();
 
-	await t.expect(getWindowInnerWidth()).gte(targetWidth, "After maximizeWindow() the window should have a greater or equal width!");
-	await t.expect(getWindowInnerHeight()).gte(targetHeight, "After maximizeWindow() the window shoudl have a greater or equal height!");
+	await t.expect(getWindowInnerWidth()).gte(
+		targetWidth,
+		"After maximizeWindow() the window should have greater or equal width!",
+	);
+	await t.expect(getWindowInnerHeight()).gte(
+		targetHeight,
+		"After maximizeWindow() the window should have greater or equal height!",
+	);
+}).after(setSizeToSomethingElse);
 
-	// there seems to be a bug in TestCafe, where resizeWindow does not work
-	// properly the first time after the window was maximized.
-	await t.resizeWindow(targetWidth, targetHeight);
-});
-
-test.skip("maximizeWindow() then resizeWindow()", async t => {
+test("maximizeWindow() then resizeWindow() - (see: https://github.com/DevExpress/testcafe/issues/8157)", async t => {
 	await t.maximizeWindow();
-	
-	const targetWidth = _targetWidth + getRandomIntInRange(1, 100);
-	const targetHeight = _targetHeight + getRandomIntInRange(1, 100);
-	
-	const getWindowInnerDimensions = ClientFunction(() => ({width: window.innerWidth, height: window.innerHeight}));
-	console.log({windowInnerDimensions: await getWindowInnerDimensions()});
+
 	await t.resizeWindow(targetWidth, targetHeight);
-	
-	await t.expect(getWindowInnerWidth()).eql(targetWidth, "Width not properly set to targetWidth!");
+
 	// this fails for native TestCafe browsers - height is off by one px
 	// testcafe-browser-provider-webdriverio-appium works around this bug
-	await t.expect(getWindowInnerHeight()).eql(targetHeight, "Height not properly set to targetHeight!");
-});
+	// (see: https://github.com/DevExpress/testcafe/issues/8157)
+	await t.expect(getWindowInnerDimensions()).eql(targetDimensions, "Window not resized properly!");
+}).after(setSizeToSomethingElse);
 
-test.skip("canResizeWindowToDimensions() - can't be implemented since we want to be able to use the same tests on fixed sized mobile devices and on resizable desktops.", async t => {
+test("canResizeWindowToDimensions() - can't be implemented since we want to be able to use the same tests on fixed sized mobile devices and on resizable desktops.", async t => {
 	// greater than 8k
-	const targetWidth = 7680 + getRandomIntInRange(1, 100);
-	const targetHeight = 4320 + getRandomIntInRange(1, 100);
+	const targetWidth = 7680 + 7;
+	const targetHeight = 4320 + 7;
+	const targetDimensions = {width: targetWidth, height: targetHeight};
 
-	await t.expect(getWindowInnerWidth()).notEql(targetWidth, "Width not expected to be targetWidth yet!");
-	await t.expect(getWindowInnerHeight()).notEql(targetHeight, "Height not expected to be targetHeight yet!");
-    
+	await t.expect(getWindowInnerDimensions()).notEql(targetDimensions, "Window not expected to be resized!");
+
 	await t.resizeWindow(targetWidth, targetHeight);
-    
-	await t.expect(getWindowInnerWidth()).notEql(targetWidth, "Width not expected to ever be targetWidth!");
-	await t.expect(getWindowInnerHeight()).notEql(targetHeight, "Height not expected to ever be targetHeight!");
-});
+
+	await t.expect(getWindowInnerDimensions()).notEql(targetDimensions, "Window not expected to ever be resized!");
+}).after(setSizeToSomethingElse);
 
 test("takeScreenshot()", async t => {
-	const targetWidth = _targetWidth;
-	const targetHeight = _targetHeight;
-
 	await t.resizeWindow(targetWidth, targetHeight);
+	await t.expect(getWindowInnerDimensions()).eql(targetDimensions, "Window not resized properly!");
 
 	const screenshotPath = `${mkValidPath(t.browser.alias)}--${mkValidPath(t.test.name)}`;
-    
 	await t.takeScreenshot(screenshotPath);
-
-	await t.expect(getWindowInnerWidth()).eql(targetWidth, "Width not properly set to targetWidth!");
-	await t.expect(getWindowInnerHeight()).eql(targetHeight, "Height not properly set to targetHeight!");
 
 	// adjust for retina displays
 	const devicePixelRatio = await getWindowDevicePixelRatio();
-	const expectedWidth = targetWidth * devicePixelRatio;
-	const expectedHeight = targetHeight * devicePixelRatio;
+	const adjustedDimensions = {
+		width: targetWidth * devicePixelRatio,
+		height: targetHeight * devicePixelRatio,
+	};
 
 	const png = PNG.sync.read(readFileSync(joinPath("screenshots", `${screenshotPath}.png`)));
-	await t.expect(png.width).eql(expectedWidth, "Screenshot does not have the same width as the window!");
-	try {
-		await t.expect(png.height).eql(expectedHeight, "Screenshot does not have the same height as the window! This is a known bug!");
-	} catch (error) {
-		await t.report(JSON.stringify(error, null, 4
-		));
-	}
-});
+	const pngDimensions = {width: png.width, height: png.height};
+	await t.expect(pngDimensions).eql(adjustedDimensions, "Screenshot does not have the same dimensions as the window!");
+}).after(setSizeToSomethingElse);
